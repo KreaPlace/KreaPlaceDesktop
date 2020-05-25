14260f6c8d1c1e0ec065dfdc692208df322bed98
WiktorrWie <55576682+WiktorrWie@users.noreply.github.com>
2020-05-25 19:10:52 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,36 @@
+        var currentTime = new Date();
+var h = currentTime.getHours();
+hourarray = ["Good night!", "Good night!", "Good night!", "Good night!", "Good night!", "Good morning!", "Good morning!", "Good morning!", "Good morning!", "Good morning!", "Good morning!", "Good morning!", "Good afternoon!", "Good afternoon!", "Good afternoon!", "Good afternoon!", "Good afternoon!", "Good afternoon!", "Good evening!","Good evening!","Good evening!", "Good evening!", "Good evening!", "Good evening!",]
+var greeting = document.getElementById("greeting").innerHTML = hourarray[h];
+
+var images = ["../photos/heart.png", "../photos/heart_red.png"]
+
+var imgState = 0;
+
+var imgTag = document.getElementById("heart");
+
+imgTag.addEventListener("click", function (event) {
+  imgState = (++imgState % images.length);
+  event.target.src = images[imgState];
+});
+
+var imgTag2 = document.getElementById("heart2");
+
+imgTag2.addEventListener("click", function (event) {
+  imgState = (++imgState % images.length);
+  event.target.src = images[imgState];
+});
+
+var imgTag3 = document.getElementById("heart3");
+
+imgTag3.addEventListener("click", function (event) {
+  imgState = (++imgState % images.length);
+  event.target.src = images[imgState];
+});
+
+var imgTag4 = document.getElementById("heart4");
+
+imgTag4.addEventListener("click", function (event) {
+  imgState = (++imgState % images.length);
+  event.target.src = images[imgState];
+});
---
+++ js/js3.js
@@ -0,0 +1,26 @@
+            var images = ["../photos/heart.png", "../photos/heart_red.png"]
+
+            var imgState = 0;
+
+            var imgTag = document.getElementById("heart");
+
+            var imgTag5 = document.getElementById("heart5");
+
+            imgTag5.addEventListener("click", function (event) {
+                imgState = (++imgState % images.length);
+                event.target.src = images[imgState];
+            });
+
+        var imgTag6 = document.getElementById("heart6");
+
+imgTag6.addEventListener("click", function (event) {
+  imgState = (++imgState % images.length);
+  event.target.src = images[imgState];
+});
+
+        var imgTag7 = document.getElementById("heart7");
+
+imgTag7.addEventListener("click", function (event) {
+  imgState = (++imgState % images.length);
+  event.target.src = images[imgState];
+});
