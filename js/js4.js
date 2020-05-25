70478c662f22ee181e40792926396f0205849603
WiktorrWie <55576682+WiktorrWie@users.noreply.github.com>
2020-05-25 23:18:12 +0200

    Add files via upload

--- js/js4.js
+++ js/js4.js
@@ -17,22 +17,3 @@ var video = document.getElementById('video');
 document.getElementById("photo").addEventListener("click", function() {
 	context.drawImage(video, 0, 0, 375, 375);
 });
-
-const select = document.getElementById('select');
-
-function gotDevices(mediaDevices) {
-  select.innerHTML = '';
-  select.appendChild(document.createElement('option'));
-  let count = 1;
-  mediaDevices.forEach(mediaDevice => {
-    if (mediaDevice.kind === 'videoinput') {
-      const option = document.createElement('option');
-      option.value = mediaDevice.deviceId;
-      const label = mediaDevice.label || `Camera ${count++}`;
-      const textNode = document.createTextNode(label);
-      option.appendChild(textNode);
-      select.appendChild(option);
-    }
-  });
-}
-navigator.mediaDevices.enumerateDevices().then(gotDevices);
