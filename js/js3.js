cc9cd6189bc1d8efc6bd8de8dfc942b6ec2ff96a
WiktorrWie <55576682+WiktorrWie@users.noreply.github.com>
2020-05-26 23:14:31 +0200

    Add files via upload

--- js/js3.js
+++ js/js3.js
@@ -1,8 +1,8 @@
             var images = ["../photos/heart.png", "../photos/heart_red.png"];
 
             var imgState = 0;
-var imgState6 = 0;
-var imgState7 = 0;
+            var imgState6 = 0;
+            var imgState7 = 0;
             var imgTag = document.getElementById("heart");
 
             var imgTag5 = document.getElementById("heart5");
@@ -12,16 +12,24 @@ var imgState7 = 0;
                 event.target.src = images[imgState];
             });
 
-        var imgTag6 = document.getElementById("heart6");
+            var imgTag6 = document.getElementById("heart6");
 
-imgTag6.addEventListener("click", function (event) {
-  imgState6 = (++imgState6 % images.length);
-  event.target.src = images[imgState6];
-});
+            imgTag6.addEventListener("click", function (event) {
+                imgState6 = (++imgState6 % images.length);
+                event.target.src = images[imgState6];
+            });
 
-        var imgTag7 = document.getElementById("heart7");
+            var imgTag7 = document.getElementById("heart7");
 
-imgTag7.addEventListener("click", function (event) {
-  imgState7 = (++imgState7 % images.length);
-  event.target.src = images[imgState7];
-});
+            imgTag7.addEventListener("click", function (event) {
+                imgState7 = (++imgState7 % images.length);
+                event.target.src = images[imgState7];
+            });
+
+            var follow = true;
+
+            function background() {
+                let el = document.getElementById('follow');
+                el.style.backgroundColor = follow ? "#67a3d9" : "";
+                follow = !follow;
+            }
